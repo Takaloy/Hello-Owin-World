3e823fdf2be752adb06a0cb30f970f12a5987696
Teck Loy Low <teckloy.low@thetrainline.com>
2015-07-17 13:27:52 +0100

    example of how to use owin

---
+++ LetsTryOwin/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+
+namespace LetsTryOwin
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "application/json";
+                return context.Response.WriteAsync("{ \"message\" : \"hello world!\"}");
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,14 +1,21 @@
 # Hello OWIN World
 Hello World with OWIN
 
-My simple app to play around with OWIN
+very simple app to play around with OWIN. 
+Literally JUST hello world.
 
 Steps:
 1. BUILD IT (obviously!). This will pull the relevant nuget packages
+
 2. command prompt into the project folder (hint, where the web.config sits)
+
 3. run "../packages/OwinHost.3.0.1/tools/OwinHost.exe"
+
 4. http://localhost:5000/
+
 5. magix!
+
+
 
 Enjoy.
 
